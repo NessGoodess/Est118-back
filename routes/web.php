236be199d41cea76ee6407d433a7b15641623f80
Nesstor <diehere.24@gmail.com>
2026-02-17 14:24:27 -0600

    update:Route deleted

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@ Route::get('/', function () {
 
 require __DIR__ . '/auth.php';
 
-Route::get('/private-image/{path}', function ($path) {
-    if (! Storage::disk('private')->exists($path)) {
-        abort(404);
-    }
-
-    return Storage::disk('private')->get($path);
-})
-    ->where('path', '.*')
-    ->middleware('auth:sanctum');
-
 
 Route::get('/private-image/{path}', [PrivateImageController::class, 'show'])
     ->where('path', '.*')
